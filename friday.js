c49d381be241b2683f81aeb37b66a9860961d8ff
Yashverma <yv3245650@gmail.comgit config --global user.name YashVerma>
2024-06-25 01:20:11 +0530

    new coomit

--- friday.js
+++ friday.js
@@ -2,7 +2,7 @@ const btn=document.querySelector("#Now");
 btn.addEventListener("click",()=>{
     alert("hello");
 });
-const btn1=document.querySelector("#btn");
+const btn1=document.querySelector("#btn1");
 btn1.addEventListener("click",()=>{
     alert("hello");
     document.getElementById("first").innerHTML="Hello new room";
@@ -10,4 +10,70 @@ btn1.addEventListener("click",()=>{
 });
 btn1.addEventListener("click",()=>{
     document.getElementById("first")
+});
+
+
+const bt2=document.querySelector("#Noow");
+bt2.addEventListener("click",()=>{
+    alert("hello");
+});
+const btn2=document.querySelector("#btn2");
+btn2.addEventListener("click",()=>{
+    alert("hello");
+    document.getElementById("Second").innerHTML="Hello new room";
+    document.getElementById("Second").style.backgroundImage="url(room1.png)";
+});
+btn2.addEventListener("click",()=>{
+    document.getElementById("Second")
+    btn2.querySelector("#Second");
+
+});
+
+const btn3=document.querySelector("#Noww");
+btn3.addEventListener("click",()=>{
+    alert("hello");
+});
+const bt3=document.querySelector("#btn3");
+bt3.addEventListener("click",()=>{
+    alert("hello");
+    document.getElementById("forth").innerHTML="Hello new room";
+    document.getElementById("forth").style.backgroundImage="url(room1.png)";
+});
+bt3.addEventListener("click",()=>{
+    document.getElementById("forth")
+    bt3.querySelector("#forth");
+
+});  
+
+
+const btn4=document.querySelector("#NNow");
+btn4.addEventListener("click",()=>{
+    alert("hello");
+});
+const bt4=document.querySelector("#btn4");
+bt4.addEventListener("click",()=>{
+    alert("hello");
+    document.getElementById("five").innerHTML="Hello new room";
+    document.getElementById("five").style.backgroundImage="url(room1.png)";
+});
+bt4.addEventListener("click",()=>{
+    document.getElementById("five")
+    bt4.querySelector("#five");
+
+});
+
+const btn5=document.querySelector("#Neow");
+btn5.addEventListener("click",()=>{
+    alert("hello");
+});
+const bt5=document.querySelector("#btn5");
+bt5.addEventListener("click",()=>{
+    alert("hello");
+    document.getElementById("Sixe").innerHTML="Hello new room";
+    document.getElementById("Sixe").style.backgroundImage="url(room1.png)";
+});
+bt5.addEventListener("click",()=>{
+    document.getElementById("Sixe")
+    bt5.querySelector("#Sixe");
+
 });
